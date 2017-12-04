7ce94f08602c9a40e84594a7bf509043fc4d4d87
MaheshNadar <maheshnadar@wohlig.com>
2017-12-04 12:50:53 +0530

    done newsletter

--- frontend/js/controllers/newsletter.js
+++ frontend/js/controllers/newsletter.js
@@ -2,12 +2,13 @@ myApp.controller('NewsLetterCtrl', function ($scope, TemplateService, Navigation
     $scope.template = TemplateService.getHTML("content/newsletter.html");
     TemplateService.title = "NewsLetter"; // This is the Title of the Website
     $scope.navigation = NavigationService.getNavigation();
+
     $scope.tabs = [{
             name: "Edition 1",
             content: [{
                     heading: "heading 1",
                     img: "img/article1.png",
-                    txt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque ex id ligula porttitor,sed fringilla mauris pharetra. Vestibulum venenatis neque eu cursus dapibus. Nam hendrerit enim eget augue commodo, sit amet molestie urna venenatis"
+                    txt: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque ex id ligula porttitor,sed fringilla mauris pharetra. Vestibulum venenatis neque eu cursus dapibus. Nam hendrerit enim eget augue commodo, sit amet molestie urna venenatis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque ex id ligula porttitor,sed fringilla mauris pharetra. Vestibulum venenatis neque eu cursus dapibus. Nam hendrerit enim eget augue commodo, sit amet molestie urna venenatis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque ex id ligula porttitor,sed fringilla mauris pharetra. Vestibulum venenatis neque eu cursus dapibus. Nam hendrerit enim eget augue commodo, sit amet molestie urna venenatis  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin pellentesque ex id ligula porttitor,sed fringilla mauris pharetra. Vestibulum venenatis neque eu cursus dapibus. Nam hendrerit enim eget augue commodo, sit amet molestie urna venenatis"
                 },
                 {
                     heading: "heading 2",
@@ -39,6 +40,8 @@ myApp.controller('NewsLetterCtrl', function ($scope, TemplateService, Navigation
         }
     ];
 
+
+    $scope.selectedNewsLetter = $scope.tabs[0];
     $scope.selectNewsLetter = function (x) {
         $scope.selectedNewsLetter = x;
         console.log($scope.selectedNewsLetter);
