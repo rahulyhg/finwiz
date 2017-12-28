a1a1de50a973f11b9d86b1ee461413e456a950d5
Aditya <aditya.ghag@wohlig.com>
2017-12-28 12:14:41 +0530

    changes in google login

--- config/passport/google.js
+++ config/passport/google.js
@@ -1,6 +1,6 @@
 global["GoogleKey"] = "AIzaSyDMgwXi6D38isibUfShc9C2mJyaHZZ2LpE";
 global["GoogleclientId"] = "1066475903870-hr9m96kvcbjfdj9f8d1nlh10mo9kca8s.apps.googleusercontent.com";
-global["GoogleclientSecret"] = "Y1xGZWrJCyemc9e_SM0MgK24";
+global["GoogleclientSecret"] = "q6G7lOBYIokkx1wMR3z7NBcs";
 
 passport.use(new GoogleStrategy({
         clientId: GoogleclientId,
