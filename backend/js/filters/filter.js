ed8176179313a8ae7f53c24070b03f2880a2de1b
Aditya <aditya.ghag@wohlig.com>
2018-01-04 17:38:27 +0530

    changes

--- backend/js/filters/filter.js
+++ backend/js/filters/filter.js
@@ -89,7 +89,7 @@ myApp.filter('serverimage', function () {
             }
 
         } else {
-            return "img/logo.png";
+            // return "img/logo.png";
         }
     };
 });
@@ -105,7 +105,7 @@ myApp.filter('downloadImage', function () {
         if (input) {
             return adminurl + "download/" + input;
         } else {
-            return "img/logo.png";
+            // return "img/logo.png";
         }
     };
 });
