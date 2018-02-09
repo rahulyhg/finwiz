0e9151d5dba1445dd06d85378abaa983a2bf3fd7
Chintan Shah <chintan@wohlig.com>
2018-02-09 16:20:45 +0530

    done

--- config/env/production.js
+++ config/env/production.js
@@ -26,7 +26,7 @@ module.exports = {
    ***************************************************************************/
 
   port: 86,
-  realHost: "http://finwiz.wohlig.co.in",
+  realHost: "https://finwiz.wohlig.co.in",
   emails: ["chintan@wohlig.com", "jagruti@wohlig.com", "tushar@wohlig.com", "chirag@wohlig.com", "harsh@wohlig.com","aditya.ghag@wohlig.com","mahesh.nadar@wohlig.com","snehal.thakare@wohlig.com","nsemktg@gmail.com","finwiznse@gmail.com","rohan.khule@wohlig.com","karan.shah@wohlig.com","manali.gada@wohlig.com"]
 
   /***************************************************************************
