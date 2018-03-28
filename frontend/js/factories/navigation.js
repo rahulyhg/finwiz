2446b1de2325b9d5e4f397f8cb6b38822ca637ea
ShaikhNargis1406 <nargis.shaikh@wohlig.com>
2018-03-28 00:55:58 -0700

    csrf token added

--- frontend/js/factories/navigation.js
+++ frontend/js/factories/navigation.js
@@ -39,38 +39,73 @@ myApp.factory('NavigationService', function ($http) {
             subnav: []
         }
     ];
-
     return {
         getNavigation: function () {
             return navigation;
         },
         apiCall: function (url, formData, callback) {
-            $http.post(adminurl + url, formData).then(function (data) {
+            $http.get('https://finwiz.wohlig.co.in/csrfToken').then(function (data) {
                 data = data.data;
-                callback(data);
+                console.log("------", data);
+                $http.defaults.headers.post['x-csrf-token'] = data._csrf;
+                $http.post(adminurl + url, formData).then(function (data) {
+                    data = data.data;
+                    callback(data);
 
+                });
             });
         },
         searchCall: function (url, formData, i, callback) {
-            $http.post(adminurl + url, formData).then(function (data) {
+            $http.get('https://finwiz.wohlig.co.in/csrfToken').then(function (data) {
                 data = data.data;
-                callback(data, i);
+                console.log("------", data);
+                $http.defaults.headers.post['x-csrf-token'] = data._csrf;
+                $http.post(adminurl + url, formData).then(function (data) {
+                    data = data.data;
+                    callback(data, i);
+                });
             });
         },
-        
-        apiCallWithData: function (url, formData, callback) {
-            $http.post(adminurl + url, formData).then(function (data) {
-                data = data.data;
-                callback(data);
 
+        apiCallWithData: function (url, formData, callback) {
+            $http.get('https://finwiz.wohlig.co.in/csrfToken').then(function (data) {
+                data = data.data;
+                //  $.ajax({
+                //     url: adminurl + url,
+                //     type: "POST",
+                //     crossDomain: true,
+                //     beforeSend: function (xhr, settings) {
+                //         xhr.setRequestHeader('X-CSRF-Token', data._csrf);
+                //         xhr.setRequestHeader('Content-Type', 'application/json');
+                //     },
+                //     xhrFields: {
+                //         withCredentials: true
+                //     },
+                //     data:formData,
+                //     success: function (resp) {
+                //         console.log("resp------",resp.data);
+                //         callback(resp);
+
+                //     }
+                // })
+                $http.defaults.headers.post['x-csrf-token'] = data._csrf;
+                $http.post(adminurl + url, formData).then(function (data) {
+                    data = data.data;
+                    callback(data);
+                });
             });
+
         },
 
         apiCallWithoutData: function (url, callback) {
-            $http.post(adminurl + url).then(function (data) {
+            $http.get('https://finwiz.wohlig.co.in/csrfToken').then(function (data) {
                 data = data.data;
-                callback(data);
-
+                console.log("------", data);
+                $http.defaults.headers.post['x-csrf-token'] = data._csrf;
+                $http.post(adminurl + url).then(function (data) {
+                    data = data.data;
+                    callback(data);
+                });
             });
         },
     };
